ca8505942023fdfbd83d6f4b1862e2a832035b66
Alexey Melnikov <individum05@gmail.com>
2015-08-21 17:26:51 +0600

    Функция вывода списка пользоватей на канале

---
+++ server/users_list.h
@@ -0,0 +1,16 @@
+#ifndef ELTEXIRC_SERVER_USERS_LIST_H_
+#define ELTEXIRC_SERVER_USERS_LIST_H_
+
+#include <stdio.h>
+#include <string.h>
+#include "irc_limits.h"
+#include "users.h"
+
+#define IRCERR_SCL_LIST 1
+#define IRCERR_SCL_WRITE 2
+
+int SendChannelList(int sock, struct IRCAllChannels *channels,
+                    struct IRCAllUsers *allusers, const char *channame,
+                    const char *host, const char *nick);
+
+#endif  // ELTEXIRC_SERVER_USERS_LIST_H_
