8b7f174f434e0a78329efe484da4a7133768c7cc
Alexey Melnikov <individum05@gmail.com>
2015-08-24 10:15:34 +0600

    Удалена функция FormChanList

--- server/connect.c
+++ server/connect.c
@@ -98,69 +98,6 @@ int FormSendMsg(char *output, char *msg, const char *nick)
   }
 }
 
-int FormChanList(struct IRCAllChannels *channels, struct IRCAllUsers *users,
-                const char *nick)
-{
-  struct NamesList list = {
-    .cnt = 0,
-    .names = NULL
-  };
-  const int kFormatSize = IRC_HOST_MAX_LENGTH + IRC_NICK_BUF_SIZE + IRC_CHANNAME_MAX_LENGTH + 3;
-  const int kPreambleSize = IRC_CHANNAME_MAX_LENGTH + IRC_NICK_MAX_LENGTH + 3;
-  const char *default_msg = ":default\n";
-  char send_msg[IRC_MSG_MAX_LENGTH];
-  char raw_msg[IRC_MSG_MAX_LENGTH];
-  char host[IRC_HOST_MAX_LENGTH];
-  char preamble[kPreambleSize];
-  char format_str[kFormatSize];
-  int index, len = 0, shift = 0, nick_len = strlen(nick);
-  int preamble_len = 0;
-  int default_len = strlen(default_msg);
-  
-  memset(&preamble, 0, kPreambleSize);
-  memset(&raw_msg, 0, IRC_MSG_MAX_LENGTH);
-  memset(&send_msg, 0, IRC_MSG_MAX_LENGTH);
-  memset(&format_str, 0, kFormatSize);
-  memset(&host, 0, IRC_HOST_MAX_LENGTH);
-  
-  gethostname(host, IRC_HOST_MAX_LENGTH - 1);
-  if (len + nick_len + 3 >= IRC_HOST_MAX_LENGTH) 
-    return -1;
-    
-  sprintf(preamble, ":%s %s ", host, nick);
-  preamble_len = strlen(preamble);
-  
-  list = GetChannelsList(channels);
-  if (list.cnt == 0) {
-    FreeNamesList(&list);
-    return 0;
-  }
-  shift = 0;
-  strncpy(raw_msg, "LIST ", 5);
-  shift += 5;
-  
-  for (index = 0; index < list.cnt; index++) {
-    len = strlen(list.names[index]);
-    if (shift + preamble_len + default_len + len + 2 < IRC_MSG_MAX_LENGTH) {
-      snprintf(format_str, kFormatSize, "%s%s %s", 
-                preamble, list.names[index], default_msg);
-      strncpy(raw_msg + shift, format_str, strlen(format_str));
-      shift += strlen(format_str);
-    } else {
-      //добавить обработку переполнения
-      FreeNamesList(&list);
-      return -1;
-    }
-  }
-  printf("%d\n", shift);
-  raw_msg[shift] = '\0';
-  if (FormSendMsg(send_msg, raw_msg, nick) == 0) 
-    SendMsgToUser(users, nick, send_msg);
-  printf("send list: %s\n", send_msg);
-  FreeNamesList(&list);
-  return 0;
-}
-
 int FormPongMsg(struct IRCAllUsers *users, char *raw_msg, char *nick)
 {
   char send_msg[IRC_MSG_MAX_LENGTH];
